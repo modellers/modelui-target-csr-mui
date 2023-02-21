04a0e30fc4f3e24f3bfbeb546c5204ce478172ff
Kjartan Jónsson <kjartan@vedur.is>
2023-02-21 18:13:41 +0000

    Rendering

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import path from "path";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
-import App from "../src/App";
+import App from "../src/AppTest";
 
 const app = express();
 
