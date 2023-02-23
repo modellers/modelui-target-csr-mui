7f955c86cabf36cd751a98c14c49da8fd61b7b13
modellers <115144400+modellers@users.noreply.github.com>
2023-02-23 07:33:19 +0000

    Reusing pipeline script

---
+++ pipeline.sh
@@ -0,0 +1,17 @@
+# Script prepping build pipelines for specific targets
+# Create SSR pipline since importing core module loads wrong module type (ESM)
+# Pull latest
+# Copy esm into root directory
+# rename esm to moduleui-core-runtime
+# delete node_modules/moduleui-core-runtime
+# export DISABLE_ESLINT_PLUGIN=true 
+# replace imports
+cd src
+find . -name AppBase.js -maxdepth 1  -type f -exec sed -i '' -e  "s/import { layout } from 'modelui-core-runtime';/import { layout } from '.\/modelui-core-runtime';/g" {} \;
+cd components/
+find . -name Components.js -maxdepth 1 -type f -exec sed -i '' -e  "s/import { layout } from 'modelui-core-runtime';/import { layout } from '..\/modelui-core-runtime';/g" {} \;
+find . -name Components.js -maxdepth 1 -type f -exec sed -i '' -e  "s/import { layout } from 'modelui-core-runtime';/import { layout } from '..\/modelui-core-runtime';/g" {} \;
+
+find . -type f -exec sed -i '' -e  "s/import { layout } from 'modelui-core-runtime';/import { layout } from '..\/..\/modelui-core-runtime';/g" {} \;
+find . -type f -exec sed -i '' -e  "s/import { structs } from 'modelui-core-runtime';/import { structs } from '..\/..\/modelui-core-runtime';/g" {} \;
+find . -type f -exec sed -i '' -e  "s/import { util } from 'modelui-core-runtime';/import { util } from '..\/..\/modelui-core-runtime';/g" {} \;
